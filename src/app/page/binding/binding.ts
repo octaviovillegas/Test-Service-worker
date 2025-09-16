ea96d1a76678f6cae3d3b3b1e152348c3d7090ff
Octavio <el.octavio.villegas@gmail.com>
2025-09-15 22:59:48 -0300

    Agregar funcionalidad de binding y cálculo de promedio en el componente Binding

--- src/app/page/binding/binding.ts
+++ src/app/page/binding/binding.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'UTNFRA-binding',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './binding.html',
   styleUrl: './binding.css'
 })
 export class Binding {
 
+  public Titulo = 'Ejemplo de binding';
+  public numeroUno:number=0;
+  public numeroDos!:number;
+  public numeroResultado?: any;
+  constructor() {}
+
+  CalcularPormedio() {
+    this.numeroResultado = (this.numeroUno + this.numeroDos) / 2;
+  }
 }
